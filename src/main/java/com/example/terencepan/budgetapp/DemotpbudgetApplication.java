0c810375634242f05bf72489c647eaecf1e7e586
azuredragoon <terencenmnpan@gmail.com>
2017-05-07 22:55:15 -0700

    Sample Budget App
    Set up

---
+++ src/main/java/com/example/terencepan/budgetapp/DemotpbudgetApplication.java
@@ -0,0 +1,12 @@
+package com.example.terencepan.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotpbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotpbudgetApplication.class, args);
+	}
+}
