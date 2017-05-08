1bb6fc63268e8a6a1d7edf4e435922d011152d7e
azuredragoon <terencenmnpan@gmail.com>
2017-05-07 23:25:08 -0700

    Added Index Controller

---
+++ src/main/java/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Terence Pan on 5/7/2017.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    String index(){
+        return "index";
+    }
+}
