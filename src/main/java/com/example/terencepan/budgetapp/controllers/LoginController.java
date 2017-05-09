324a8284a24b2aa254b211c6f74c41d012cf9e40
azuredragoon <terencenmnpan@gmail.com>
2017-05-09 00:39:45 -0700

    Added Login and Register page skeletons

--- src/main/java/com/example/terencepan/budgetapp/controllers/LoginController.java
+++ src/main/java/com/example/terencepan/budgetapp/controllers/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    String index(){
-        return "index";
+    String login(){
+        return "login";
     }
 }
