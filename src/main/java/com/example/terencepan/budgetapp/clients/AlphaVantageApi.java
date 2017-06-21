4f9d7c70c0f8ada63f3bd8b347bf3b146aa9a194
azuredragoon <terencenmnpan@gmail.com>
2017-06-20 17:02:34 -0700

    Added some classes

---
+++ src/main/java/com/example/terencepan/budgetapp/clients/AlphaVantageApi.java
@@ -0,0 +1,7 @@
+package com.example.terencepan.budgetapp.clients;
+
+/**
+ * Created by Terence Pan on 6/20/2017.
+ */
+public class AlphaVantageApi {
+}
---
+++ src/main/java/com/example/terencepan/budgetapp/entities/Stock.java
@@ -0,0 +1,10 @@
+package com.example.terencepan.budgetapp.entities;
+
+import javax.persistence.Entity;
+/**
+ * Created by Terence Pan on 6/20/2017.
+ */
+public class Stock {
+
+
+}
